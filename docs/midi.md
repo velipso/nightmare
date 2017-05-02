e08bf56b8c8bd208f1dcfaf89fc9f560ee256a9c
Sean <voidqk@users.noreply.github.com>
2017-05-02 16:19:00 -0400

    working on analyzing data and writing impl doc

---
+++ docs/midi.md
@@ -0,0 +1,139 @@
+MIDI File Format
+================
+
+This document is my attempt at simplifying and clairifying the MIDI file format.
+
+I've used three primary sources of information:
+
+1. [Official MIDI specification](https://midi.org)
+2. [Yamaha XF Format specification](http://www.jososoft.dk/yamaha//pdf/xfspec.pdf)
+3. Real-world MIDI files
+
+My intention is to document what a MIDI file *should* look like (for encoders), but also what
+real-world data looks like (for lenient decoders).
+
+Real-World Data
+---------------
+
+Thankfully, there exists a large body of real-world MIDI files to inspect:
+
+[The Geocities MIDI Collection (2009)](https://archive.org/details/archiveteam-geocities-midi-collection-2009)
+
+This archive has 48,197 MIDI files, totalling 1.3 GB of data.
+
+The data also happens to be pretty damn dirty too -- which helps us in figuring out good strategies
+for recovering useful information.
+
+MIDI Overview
+-------------
+
+MIDI is a protocol for transmitting note and controller information across a serial connection.  A
+good mental model is to think of MIDI as the data that comes off a music keyboard *before* being
+synthesized into sound.  For example, there is a Note On message, which indicates that a key has
+been pressed, and a Note Off message when it's released.
+
+The MIDI protocol is designed to be used in real-time -- as the musician is generating events with
+the keyboard, the data is being transmitted over the wire, which a synthesizer can react to in
+real-time by outputting sound.
+
+In order to store a stream of MIDI messages in a file, messages are timestamped.  This allows a
+program to "play back" the stream of messages at the correct time.
+
+The bulk of the complexity is understanding the messages themselves.  The MIDI protocol has
+maintained backwards compatibility, which means it's easy to see the raw bytes that make up a
+message, but understanding how to unpack and interpret each message is complicated.
+
+General MIDI 2 is an additional layer of requirements on top of the basic MIDI protocol that further
+specifies the meaning behind messages, and systems/devices claiming to support GM 2 are required to
+support a minimum feature set.
+
+Chunks
+------
+
+MIDI files consist of a stream of chunks.  Each chunk has the format:
+
+| Name       | Size                 | Description                                                 |
+|------------|----------------------|-------------------------------------------------------------|
+| Chunk Type | 4 bytes              | Format of the chunk, typically either `'MThd'` or `'MTrk'`  |
+| Data Size  | 4 bytes (big endian) | Length of the data (not including Chunk Type of Data Size)  |
+| Data       | Specified by Data Size | Raw data for the chunk, interpreted based on Chunk Type   |
+
+A MIDI file start with a `'MThd'` chunk, followed by one or more `'MTrk'` chunks.  I've also
+seen `'XFIH'` and `'XFKM'` chunks, which after searching online, shows they are extensions by
+Yamaha for karaoke.
+
+Chunks *should* be immediately one after another, and most files follow that rule -- however, a few
+files have junk data between chunks (for example, a string of repeated bytes like `00 00 00 ...`).
+A robust decoder might want to search the area around the end of a chunk for the next chunk,
+skipping over repeated byte data, including searching backwards in case the previous chunk reported
+a Data Size too large.
+
+The chunk stream starts with `'MThd'`, and *should* only contain `'MThd'` once.  In practice, all
+MIDI files I've seen start with `'MThd'`, but some also have `'MThd'` chunks in the middle of the
+file.  I suspect this should be treated like two MIDI files have been concatenated together into
+one.
+
+The largest Data Size I've seen is 362,528 bytes (`dou_01.mid`), which is 45 minutes of dense piano.
+A good sanity check might be to make sure the Data Size isn't larger than, say, 1,000,000 bytes,
+otherwise you're probably reading bad data.
+
+### Chunk Statistics
+
+The real-world data contains:
+
+* 48,197 MIDI files, all of them starting with an `'MThd'` chunk
+* 539,469 total recognizable chunks inside those files
+    * 124 of all chunks were misaligned and found by searching near where a chunk should start
+    * 19 of all chunks were `'MThd'` chunks in the middle of a file
+* 1,767 files had garbage data at the end that couldn't be recognized as a chunk
+
+Header Chunk `'MThd'`
+---------------------
+
+The header chunk has a type of `'MThd'` (`4D 54 68 64`).  The size should always be 6 bytes long
+(i.e., Data Size is `00 00 00 06`).
+
+| Name         | Size                 | Description                        |
+|--------------|----------------------|------------------------------------|
+| Format       | 2 bytes (big endian) | `0`, `1`, or `2`, described below  |
+| Track Chunks | 2 bytes (big endian) | Number of track chunks in the file |
+| Division     | 2 bytes (big endian) | Timestamp format, described below  |
+
+### Format
+
+The Format can either be `00 00`, `00 01`, or `00 02`, each with the following meanings:
+
+| Format  | Description                                                                           |
+|---------|---------------------------------------------------------------------------------------|
+| `00 00` | The file contains a single multi-channel track (so Track Chunks should be `00 01`)    |
+| `00 01` | The file contains one or more tracks that should be played simultaneously             |
+| `00 02` | The file contains one or more tracks that are independant of each other               |
+
+### Track Chunks
+
+The Track Chunks should be a count of `'MTrk'` chunks in the file.  In practice, this number is
+sometimes wrong.  A decoder should probably just ignore it, because the `'MTrk'` chunks can be
+counted directly.
+
+When multiple `'MThd'` chunks are in a file (which is against the specification), sometimes the
+Track Count represents the tracks up until that point, and sometimes they represent the total number
+of track chunks in the entire file.  And sometimes it's just completely wrong.  Yet another reason
+to ignore the field.
+
+### Division
+
+TODO: THIS
+
+### `'MThd'` Statistics
+
+* One file (`candyluv.mid`) has a header with Data Size set to 10 (`00 00 00 0A`)
+* Distribution of Formats:
+    * Format 0: 13.5% (6529 of 48216)
+    * Format 1: 86.4% (41662 of 48216)
+    * Format 2: 0.05% (23 of 48216)
+    * Bad Format: 0.004% (2 of 48216, `boythorn.mid` has `74 01` and `possible.mid` has `70 01`)
+* 0.74% of files have an incorrect Track Chunks count (359 of 48197)
+
+### Track Chunks
+
+TODO: MORE
--- src/demo/main.c
+++ src/demo/main.c
@@ -5,9 +5,292 @@
 #include "SDL.h"
 #include "../nightmare.h"
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+typedef void *(*nm_alloc_func)(size_t sz);
+typedef void (*nm_free_func)(void *ptr);
+
+nm_alloc_func nm_alloc = malloc;
+nm_free_func nm_free = free;
+
+static bool fs_isdir(const char *dir){
+	struct stat buf;
+	if (stat(dir, &buf) != 0)
+		return 0;
+	return S_ISDIR(buf.st_mode);
+}
+
+static bool fs_isfile(const char *file){
+	FILE *fp = fopen(file, "rb");
+	if (fp == NULL)
+		return false;
+	fclose(fp);
+	return true;
+}
+
+typedef void (*fs_readdir_each_func)(const char *file);
+static void fs_readdir(const char *dir, fs_readdir_each_func f_each){
+	DIR *dp;
+	struct dirent *ep;
+	dp = opendir(dir);
+	int rslen = strlen(dir);
+	if (rslen >= 4000)
+		return;
+	char full[4001];
+	strcpy(full, dir);
+	if (dp != NULL){
+		while (true){
+			ep = readdir(dp);
+			if (ep == NULL)
+				break;
+			if (strcmp(ep->d_name, ".") == 0 || strcmp(ep->d_name, "..") == 0)
+				continue;
+			int dlen = strlen(ep->d_name);
+			if (rslen + dlen + 1 >= 4000)
+				continue;
+			full[rslen] = '/';
+			strcpy(&full[rslen + 1], ep->d_name);
+			f_each(full);
+		}
+		closedir(dp);
+	}
+}
+
+void process_midi(const char *file);
+void each_midi(const char *file){
+	if (fs_isdir(file)){
+		fs_readdir(file, each_midi);
+		return;
+	}
+	int len = strlen(file);
+	if (strcmp(&file[len - 4], ".mid") == 0 ||
+		strcmp(&file[len - 4], ".MID") == 0 ||
+		strcmp(&file[len - 4], ".Mid") == 0)
+		process_midi(file);
+}
+
+uint32_t read_u32_be(FILE *fp){
+	uint32_t c1 = fgetc(fp);
+	uint32_t c2 = fgetc(fp);
+	uint32_t c3 = fgetc(fp);
+	uint32_t c4 = fgetc(fp);
+	return (c1 << 24) | (c2 << 16) | (c3 << 8) | c4;
+}
+
+uint32_t mem_u16_be(uint8_t *mem){
+	uint32_t c1 = mem[0];
+	uint32_t c2 = mem[1];
+	return (c1 << 8) | c2;
+}
+
+uint32_t mem_u32_be(uint8_t *mem){
+	uint32_t c1 = mem[0];
+	uint32_t c2 = mem[1];
+	uint32_t c3 = mem[2];
+	uint32_t c4 = mem[3];
+	return (c1 << 24) | (c2 << 16) | (c3 << 8) | c4;
+}
+
+typedef struct {
+	uint32_t type;
+	uint32_t size;
+	uint8_t *data;
+} midi_chunk;
+
+const char *curfile;
+
+bool is_chunk_type(uint32_t type){
+	bool isch =
+		type == 0x4D546864 || // MThd
+		type == 0x4D54726B || // MTrk
+		type == 0x58464948 || // XFIH  XF Information Header
+		type == 0x58464B4D || // XFKM  XF Karaoke Message
+		false;
+	if (!isch && false){
+		uint8_t c1 = type & 0xFF;
+		uint8_t c2 = (type >> 8) & 0xFF;
+		uint8_t c3 = (type >> 16) & 0xFF;
+		uint8_t c4 = type >> 24;
+		if (c1 >= 0x20 && c1 <= 0x7F &&
+			c2 >= 0x20 && c2 <= 0x7F &&
+			c3 >= 0x20 && c3 <= 0x7F &&
+			c4 >= 0x20 && c4 <= 0x7F)
+			printf("Unknown chunk: '%c%c%c%c' %s\n", c4, c3, c2, c1, curfile);
+	}
+	return isch;
+}
+
+bool is_repeat_byte(uint32_t type){
+	uint8_t c1 = type & 0xFF;
+	uint8_t c2 = (type >> 8) & 0xFF;
+	uint8_t c3 = (type >> 16) & 0xFF;
+	uint8_t c4 = type >> 24;
+	return c1 == c2 && c1 == c3 && c1 == c4;
+}
+
+int aligned_chunks = 0;
+int misaligned_chunks = 0;
+int bad_chunks = 0;
+int garbage_end = 0;
+int all_files = 0;
+int mid_mthd = 0;
+int mtrk_chunks = 0;
+int64_t total_size = 0;
+
+bool read_midi_chunk(FILE *fp, midi_chunk *chk){
+	// first, just try to read the chunk immediately
+	uint32_t type = read_u32_be(fp);
+	if (is_chunk_type(type))
+		aligned_chunks++;
+	else{
+		// search around for the chunk
+
+		// chop off any repeated bytes
+		while (is_repeat_byte(type) && !feof(fp))
+			type = read_u32_be(fp);
+		if (feof(fp))
+			return false;
+
+		// we can move 11 bytes backwards safely
+		// -4 to unread the type, then -7 is still safe because the minimum chunk size is 8 bytes
+		// and we don't want to be stuck in an infinite loop
+		long start = ftell(fp);
+		if (start < 11)
+			return false;
+		start -= 11;
+		fseek(fp, start, SEEK_SET);
+		uint8_t typewin[32] = {0};
+		fread(typewin, 1, 32, fp);
+		bool found = false;
+		for (int i = 0; i < 28; i++){
+			type = mem_u32_be(&typewin[i]);
+			if (is_chunk_type(type)){
+				misaligned_chunks++;
+				found = true;
+				fseek(fp, start + i + 4, SEEK_SET);
+				break;
+			}
+		}
+		if (!found){
+			bad_chunks++;
+			return false;
+		}
+	}
+
+	uint32_t size = read_u32_be(fp);
+	if (size > 1000000 || feof(fp))
+		size = 0;
+	uint8_t *data = NULL;
+	if (size > 0){
+		data = nm_alloc(sizeof(uint8_t) * size);
+		if (data == NULL)
+			return false;
+		size = fread(data, 1, size, fp);
+	}
+	chk->type = type;
+	chk->size = size;
+	chk->data = data;
+	return true;
+}
+
+void process_midi_mthd(midi_chunk chk){
+	if (chk.size < 6){
+		printf("MThd chunk size too small: %s\n", curfile);
+		return;
+	}
+	int format = ((int)chk.data[0] << 8) | chk.data[1];
+	int division = ((int)chk.data[4] << 8) | chk.data[5];
+}
+
+void process_midi_mtrk(midi_chunk chk){
+}
+
+void process_midi(const char *file){
+	FILE *fp = fopen(file, "rb");
+	if (fp == NULL){
+		fprintf(stderr, "Failed to read file: %s\n", file);
+		exit(1);
+		return;
+	}
+
+	curfile = &file[32];
+
+	fseek(fp, 0L, SEEK_END);
+	long filesize = ftell(fp);
+	fseek(fp, 0L, SEEK_SET);
+
+	midi_chunk chk;
+	if (!read_midi_chunk(fp, &chk)){
+		fclose(fp);
+		return;
+	}
+	if (chk.type != 0x4D546864) // MThd
+		printf("MThd isn't always first chunk: %s\n", curfile);
+	else
+		process_midi_mthd(chk);
+	nm_free(chk.data);
+
+	//
+	// at this point, assume we are dealing with a genuine MIDI file
+	//
+
+	all_files++;
+	total_size += filesize;
+
+	int found_tracks = 0;
+	while (true){
+		int testc = fgetc(fp);
+		if (testc == EOF)
+			break;
+		ungetc(testc, fp);
+		if (!read_midi_chunk(fp, &chk)){
+			garbage_end++;
+			break;
+		}
+		if (chk.type == 0x4D546864){
+			mid_mthd++;
+			process_midi_mthd(chk);
+		}
+		else if (chk.type == 0x4D54726B){
+			mtrk_chunks++;
+			process_midi_mtrk(chk);
+		}
+		nm_free(chk.data);
+	}
+	fclose(fp);
+}
+
 int main(){
-	SDL_Init(SDL_INIT_EVERYTHING);
-	nm_test();
-	SDL_Quit();
+	//SDL_Init(SDL_INIT_AUDIO);
+	//nm_test();
+	//SDL_Quit();
+
+	const char *root = "/Users/sean/Downloads/midi/data";
+	fs_readdir(root, each_midi);
+	//process_midi("/Users/sean/Downloads/midi/data/f/For_Those_About_To_Rock.MID");
+	//process_midi("/Users/sean/Downloads/midi/data/c/cantina13.mid");
+	//process_midi("/Users/sean/Downloads/midi/data/q/qfg4open.mid");
+
+	printf(
+		"-----------------------\n"
+		"MIDI Files       : %d\n"
+		"Total File Size  : %g GB\n"
+		"All chunks       : %d\n"
+		"Misaligned       : %d\n"
+		"Garbage end      : %d\n"
+		"MThd in middle   : %d\n",
+		all_files,
+		(double)total_size / (1024.0 * 1024.0 * 1024.0),
+		aligned_chunks + misaligned_chunks,
+		misaligned_chunks,
+		garbage_end,
+		mid_mthd);
+
+
 	return 0;
 }
