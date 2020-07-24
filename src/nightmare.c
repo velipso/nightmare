788cbbc10bd1c5797b44afa17e33336762ce8c9c
Sean <velipso@users.noreply.github.com>
2020-07-24 12:08:37 -0400

    testing commit

--- src/nightmare.c
+++ src/nightmare.c
@@ -3,3 +3,9 @@
 // Project Home: https://github.com/velipso/nightmare
 
 #include "nightmare.h"
+
+#include <stdlib.h>
+
+sink_malloc_f  sink_malloc  = malloc;
+sink_realloc_f sink_realloc = realloc;
+sink_free_f    sink_free    = free;
--- src/nightmare.h
+++ src/nightmare.h
@@ -5,4 +5,16 @@
 #ifndef NIGHTMARE__H
 #define NIGHTMARE__H
 
+#include <stdint.h>
+
+typedef void *(*nm_malloc_f)(size_t sz);
+typedef void *(*nm_realloc_f)(void *ptr, size_t sz);
+typedef void (*nm_free_f)(void *ptr);
+
+// all memory management is through these functions, which default to stdlib's malloc/realloc/free
+// overwrite these global variables with your own functions if desired
+extern nm_malloc_f  nm_malloc;
+extern nm_realloc_f nm_realloc;
+extern nm_free_f    nm_free;
+
 #endif // NIGHTMARE__H
